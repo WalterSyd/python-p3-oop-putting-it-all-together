846acfe8228b1cf54270d38fd5a6d9fcf309ed61
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-05 16:01:34 +0300

    Ensured all tests are passing

--- lib/shoe.py
+++ lib/shoe.py
@@ -2,32 +2,33 @@
 
 class Shoe:
     
-    def __init__(self, brand, size, condition="New"):
+    def __init__(self, brand, size):
         self.brand = brand
         self.size = size
-        self.condition = None
+        self.condition = "New"
 
     @property
     def size(self):
         return self._size
     
     @size.setter
-    def size(self,value):
+    def size(self, value):
         if not isinstance(value, int):
-            print("size must be an integer", end="")
+            print("size must be an integer\n", end="")
         else:
             self._size = value
 
 
     def cobble(self):
-          print("your shoe is as good as new!\n", end="")
-          return self
+          print("Your shoe is as good as new!\n", end="")
+         
 
-    def repair(self):
+    def makes_new(self):
         self.condition = "New"
 
 
 shoe1 = Shoe("Nike",10)
 # print(shoe1.brand)
 # print(shoe1.size)
+print(shoe1.condition)
 print(shoe1.cobble())
